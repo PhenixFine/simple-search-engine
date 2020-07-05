e615ba3f2f6079f7b5437cc21bdee8bcfe9d4df6
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-07-05 18:36:50 -0400

    Stage 3 of 6 for JetBrains Academy Kotlin Simple Search Engine project. • array everyone was made a top level array so other functions could access it • the initializing of the array everyone was moved to initialize() • menu() was created for stage 3 to give the user the option to search the array, print the array, or to exit. • the inner part of the repeat that was in main() was moved to find() • printAll() was added to print everything within the array.

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,47 @@
+private lateinit var EVERYONE: Array<String>
+
 fun main() {
+    initialize()
+    menu()
+}
+
+fun initialize() {
     val people = getNum("Enter the number of people:")
     println("Enter all people:")
-    val everyone: Array<String> = Array(people) { readLine()!! }
-    println()
-    val searchNum = getNum("Enter the number of search queries:")
+    EVERYONE = Array(people) { readLine()!! }
+}
 
-    repeat(searchNum) {
-        println()
-        val search = getString("Enter data to search people:").trim()
-        println()
-        var found = false
-        everyone.forEach {
-            if (it.toLowerCase().contains(search.toLowerCase())) {
-                if (!found) {
-                    println("Found people:")
-                    found = true
-                }
-                println(it)
-            }
+fun menu() {
+    val menu = "\n=== Menu ===\n1. Find a person\n2. Print all people\n0. Exit"
+    var num: Int
+    var exit = false
+    do {
+        val read = getString(menu)
+        num = read.toIntOrNull() ?: getNum(read, true)
+        when (num) {
+            1 -> find()
+            2 -> printAll()
+            0 -> exit = true
+            else -> println("\nIncorrect option! Try again.")
         }
-        if (!found) println("No matching people found.")
+    } while (!exit)
+}
+
+fun find() {
+    val search = getString("\nEnter a name or email to search all suitable people.").trim()
+    var found = false
+    EVERYONE.forEach {
+        if (it.toLowerCase().contains(search.toLowerCase())) {
+            if (!found) found = true
+            println(it)
+        }
     }
+    if (!found) println("No matching people found.")
+}
+
+fun printAll() {
+    println("\n=== List of people ===")
+    EVERYONE.forEach { println(it) }
 }
 
 fun getNum(text: String, defaultMessage: Boolean = false): Int {
