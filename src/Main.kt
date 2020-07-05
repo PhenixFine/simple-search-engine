df36da3e64bc28707db0e5e4c1bc492a3fe31647
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-07-05 15:43:42 -0400

    Stage 2 of 6 for JetBrains Academy Kotlin Simple Search Engine project. • previous stage code was removed for stage 2 • val people gets the Int  number of people to be entered • val everyone is an array of strings initialized to the size of people and stores a string for each line entered from the user. • val searchNum holds the Int number of searches the user wishes to do for the entered data. • a repeat loop loops for the number of searches the user wishes to do • string search, temporarily stores each word or words the user wants to find • boolean found is set to false for each loop, and is set to true if a search is found. • each string within the array of everyone is searched and prints any strings found and if none is found a statement is printed indicating so, making use of the boolean found. • getNum() - was added to get a number from the user, using getString() to get the input. It also verifies that the user entered a number with isNumber(), if the user didn't it will loop until the user does enter a number. • getString() - prints a given line for the user to read, and then returns the user input as a String. • isNumber() - a boolean that verifies if a given string is a number.

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,44 @@
 fun main() {
-    val words = readLine()!!.split(" ").toTypedArray()
-    val search = readLine()!!
-    println(if (words.contains(search)) words.indexOf(search) + 1 else "Not Found")
+    val people = getNum("Enter the number of people:")
+    println("Enter all people:")
+    val everyone: Array<String> = Array(people) { readLine()!! }
+    println()
+    val searchNum = getNum("Enter the number of search queries:")
+
+    repeat(searchNum) {
+        println()
+        val search = getString("Enter data to search people:").trim()
+        println()
+        var found = false
+        everyone.forEach {
+            if (it.toLowerCase().contains(search.toLowerCase())) {
+                if (!found) {
+                    println("Found people:")
+                    found = true
+                }
+                println(it)
+            }
+        }
+        if (!found) println("No matching people found.")
+    }
 }
+
+fun getNum(text: String, defaultMessage: Boolean = false): Int {
+    val strErrorNum = " was not a number, please try again: "
+    var num = text
+    var default = defaultMessage
+
+    do {
+        num = getString(if (default) num + strErrorNum else num)
+        if (!default) default = true
+    } while (!isNumber(num))
+
+    return num.toInt()
+}
+
+fun getString(text: String): String {
+    println(text)
+    return readLine()!!
+}
+
+fun isNumber(number: String) = number.toIntOrNull() != null
