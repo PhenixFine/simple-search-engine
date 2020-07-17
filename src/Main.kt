4d769f4dc43db695f732cb6d09b19f5cfca545a3
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-07-17 16:07:40 -0400

    Stage 5 of 6 for JetBrains Academy Kotlin Simple Search Engine project. • accidentally skipped committing stage 4: it was just adding the ability to receive a file with command line argument, which is received through the main() args Array • mutableMap EVERYONE_MAP was added for a reverse index of the Array EVERYONE to reduce search time. • main() now receives a file to be loaded through the args Array ( I included the file names.txt as a sample that I pulled from their test parameters ). • initialize() was changed to load each line into EVERYONE and each word is indexed into EVERYONE_MAP • menu() was changed to account for input no longer being asked from user. val num was changed to lambda function for returning a number from the string read. • find() was updated to make use of using EVERYONE_MAP for quickly finding a match.

--- src/Main.kt
+++ src/Main.kt
@@ -1,42 +1,66 @@
-private lateinit var EVERYONE: Array<String>
+import java.io.File
 
-fun main() {
-    initialize()
-    menu()
+private lateinit var EVERYONE: Array<String>
+private val EVERYONE_MAP = mutableMapOf<String, MutableList<Int>>()
+
+fun main(args: Array<String>) {
+    var fileFound = true
+    try {
+        if (args[0] != "--data" && !File(args[1]).isFile) fileFound = false
+    } catch (e: Exception) {
+        fileFound = false
+    }
+    if (fileFound) {
+        initialize(args[1])
+        menu()
+    } else {
+        print(if (args.isNotEmpty() && args.size == 2) "Loading ${args[1]} failed." else "No filename provided.")
+        println(" Please try again.\n")
+    }
 }
 
-fun initialize() {
-    val people = getNum("Enter the number of people:")
-    println("Enter all people:")
-    EVERYONE = Array(people) { readLine()!! }
+fun initialize(fileName: String) {
+    val muteLines = mutableListOf<String>()
+    var count = 0
+
+    File(fileName).forEachLine {
+        muteLines.add(it)
+        var hold = it.trim()
+        while (hold.contains("  ")) hold = hold.replace("  ", " ")
+        val split = hold.toLowerCase().split(" ").toTypedArray()
+        for (word in split) {
+            if (EVERYONE_MAP.containsKey(word)) {
+                EVERYONE_MAP[word]?.add(count)
+            } else EVERYONE_MAP[word] = mutableListOf(count)
+        }
+        count++
+    }
+    EVERYONE = muteLines.toTypedArray()
 }
 
 fun menu() {
-    val menu = "\n=== Menu ===\n1. Find a person\n2. Print all people\n0. Exit"
-    var num: Int
+    val menu = "=== Menu ===\n1. Find a person\n2. Print all people\n0. Exit"
+    var read = getString(menu)
+    val num = { read.toIntOrNull() ?: getNum(read, true) }
     var exit = false
-    do {
-        val read = getString(menu)
-        num = read.toIntOrNull() ?: getNum(read, true)
-        when (num) {
+
+    while (!exit) {
+        when (num()) {
             1 -> find()
             2 -> printAll()
             0 -> exit = true
             else -> println("\nIncorrect option! Try again.")
         }
-    } while (!exit)
+        if (!exit) read = getString("\n$menu")
+    }
 }
 
 fun find() {
-    val search = getString("\nEnter a name or email to search all suitable people.").trim()
-    var found = false
-    EVERYONE.forEach {
-        if (it.toLowerCase().contains(search.toLowerCase())) {
-            if (!found) found = true
-            println(it)
-        }
-    }
-    if (!found) println("No matching people found.")
+    val search = getString("\nEnter a name or email to search all suitable people.").toLowerCase().trim()
+    if (EVERYONE_MAP.containsKey(search)) {
+        println("${EVERYONE_MAP[search]?.size} persons found:")
+        EVERYONE_MAP[search]?.forEach { println(EVERYONE[it]) }
+    } else println("No matching people found.")
 }
 
 fun printAll() {
